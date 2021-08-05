17d74fffea61af27b8b78fc7b92418d0b9f8b7f6
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-08-05 20:47:26 +0530

    created some components and installed angular medium

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    DashboardComponent,
+    TasksComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
